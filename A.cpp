cec3ecca902fc07f85f6b1a728599c909348ea9d
1-Mak <110036040+1-Mak@users.noreply.github.com>
2024-03-23 21:01:33 +0300

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+int main()
+
+{
+    int length;
+    std::cin >> length;
+    typedef std::unordered_set<int> IntSet;
+    IntSet coll;
+    int number = 0;
+    for (int i = 0; i < length; i++) {
+        std::cin >> number;
+        coll.insert(number);
+
+    }
+    std::cout << coll.size();
+}
